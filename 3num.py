d1f0b4be3b7010940687c410a216b71ee2296938
pratik-prajn <pratikprajn007@gmail.com>
2023-12-23 23:18:59 +0530

    ss

--- 3num.py
+++ 3num.py
@@ -1,41 +1,8 @@
 t=int(input())
 for i in range(t):
     n=int(input())
-    a=list(map(int,input().split()))
-    b=list(map(int,input().split()))
-    c=list(map(int,input().split()))
-    
-    
-    ind=[]
-    
-    days=0
-    temp=a[0]
-    for i in range(1,n):
-        if i not in ind:
-            days+=max(a[i],temp)   
-            temp=max(a[i],temp) 
-    i=a.index(max(a))
-    ind.append(i)
-    temp2=b[0]
-    for j in range(1,n):
-        if j not in ind:
-            days+=max(b[j],temp2)   
-            temp=max(b[j],temp2) 
-    
-    temp2=b[0]
-    for j in range(1,n):
-        if j not in ind:
-            days+=max(b[j],temp2)   
-            temp=max(b[j],temp2) 
-    ii=b.index(temp2)
-    ind.append(ii)
-    
-    
-    
-    temp3=c[0]
-    for j in range(1,n):
-        if j not in ind:
-            days+=max(c[j],temp3)   
-            temp=max(c[j],temp3) 
-    iii=b.index(temp3)
-    ind.append(iii)
+    ans=0
+    for i in range(n):
+        x,y=map(int,input().split())
+        if
+        
